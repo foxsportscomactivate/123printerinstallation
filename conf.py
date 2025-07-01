348029698e51321c1fedede5101f92b2265ecacc
foxsportscomactivate <radhjain1@gmail.com>
2025-07-01 17:15:50 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = '123 HP Smart'
 
 
 # The full version, including alpha/beta/rc tags
